fd9b890cbbd1269a22a279caf47459ca2fc546a1
Brymer-Meneses <meneses.brymer@gmail.com>
2021-04-08 03:27:21 +0800

    Expose power to __init__.py and rename the function name for calling this method

--- synapse/__init__.py
+++ synapse/__init__.py
@@ -1,7 +1,8 @@
 
 from synapse.autograd.tensor import Tensor
-from synapse.autograd.__ops import matmul
+from synapse.autograd.__ops import matmul, power
 from synapse.autograd.__tensorFunctions import TensorFunction, TensorBinaryFunction
 
 from synapse.utils.gradMode import GradState, NoGrad
+
 
