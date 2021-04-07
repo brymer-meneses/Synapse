cda9f3685147adc8eb2aa0ba14db6dd4b2bf49d1
Brymer-Meneses <meneses.brymer@gmail.com>
2021-04-08 03:19:09 +0800

    Move test folder to root directory for better access

---
+++ tests/activation_test.py
@@ -0,0 +1,36 @@
+import pytest
+from unittest import TestCase
+
+from synapse import Tensor
+from synapse.nn.activations import Tanh, ReLU
+from synapse.nn.loss import MSE
+
+import numpy as np
+from numpy.testing import assert_array_equal
+
+class TestActivations(TestCase):
+
+
+    def testTanh(self):
+        tanh = Tanh()
+        testData = Tensor(np.random.uniform(-10, 10, size=(5,5)), requiresGrad =True)
+        y = tanh(testData)
+
+        initialGrad = Tensor(np.ones_like(testData.data))
+        y.backward(initialGrad)
+
+        assert_array_equal(y.data, np.tanh(testData.data))
+        assert_array_equal(testData.grad.data, 1 - np.tanh(testData.data)**2)
+
+        return
+    def testReLU(self):
+        relu = ReLU()
+        testData = Tensor(np.random.uniform(-10, 10, size=(5,5)), requiresGrad=True)
+        correctResult = np.where(testData.data > 0, testData.data, 0)
+        y = relu(testData)
+
+        initialGrad = Tensor(np.ones_like(testData.data))
+        y.backward(initialGrad)
+
+        assert_array_equal(y.data, correctResult)
+        assert_array_equal(testData.grad.data, np.where(testData.data<0, 0, 1))
---
+++ tests/loss_test.py
@@ -0,0 +1,22 @@
+import pytest
+from unittest import TestCase
+
+import synapse as sn
+import numpy as np
+from numpy.testing import assert_array_equal
+from synapse.nn.loss import MSE
+
+class TestLoss(TestCase):
+    def testMSE(self):
+        mse = MSE()
+        data1 = np.random.uniform(-10, 10, size=(3,3))
+        data2 = np.random.uniform(-10, 10, size=(3,3))
+        a = sn.Tensor(data1, True)
+        b = sn.Tensor(data2, False)
+        c = mse(a, b)
+        c.backward()
+
+        realGrad = np.multiply(2, (data1 - data2).mean())
+
+
+        assert_array_equal(a.grad.data, realGrad)
